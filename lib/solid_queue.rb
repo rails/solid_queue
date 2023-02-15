d7fdedf8f3a7fb6e9f98e942abf0c4cbcede8489
Rosa Gutierrez <rosa@37signals.com>
2023-02-15 10:11:01 +0100

    Move everything that should live under lib/ there, from app/models/

--- lib/solid_queue.rb
+++ lib/solid_queue.rb
@@ -3,5 +3,11 @@ require "solid_queue/engine"
 
 require "active_job/queue_adapters/solid_queue_adapter"
 
+require "solid_queue/configuration"
+require "solid_queue/runnable"
+require "solid_queue/dispatcher"
+require "solid_queue/scheduler"
+require "solid_queue/manager"
+
 module SolidQueue
 end
