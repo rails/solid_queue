940d7c78eb62ec2ff439e52df014969881c21979
Rosa Gutierrez <rosa@37signals.com>
2023-02-17 11:13:48 +0100

    Ensure priority is assumed from job when creating executions
    Since priority takes a default value of 0, we weren't updating it in
    any case when the execution was being created from a job with a different
    priority, because it already had a value.

--- app/models/solid_queue/execution.rb
+++ app/models/solid_queue/execution.rb
@@ -6,7 +6,6 @@ class SolidQueue::Execution < ActiveRecord::Base
   private
     def assume_attributes_from_job
       self.queue_name ||= job&.queue_name
-      self.priority ||= job&.priority
+      self.priority = job&.priority if job&.priority.to_i > priority
     end
-
 end
