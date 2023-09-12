41f1ea684c7b358603cd114c178122393a64b734
Rosa Gutierrez <rosa@37signals.com>
2023-09-12 10:15:31 +0200

    Add a simple Queue model to wrap some queue-like operations
    Stub pause operations for now, as we'll support pause later on.

---
+++ app/models/solid_queue/queue.rb
@@ -0,0 +1,48 @@
+module SolidQueue
+  class Queue
+    attr_accessor :name
+
+    class << self
+      def all
+        SolidQueue::Job.select(:queue_name).distinct.collect do |job|
+          new(job.queue_name)
+        end
+      end
+
+      def find_by_name(name)
+        new(name)
+      end
+    end
+
+    def initialize(name)
+      @name = name
+    end
+
+    def paused?
+      false
+    end
+
+    def pause
+    end
+
+    def resume
+    end
+
+    def clear
+      Job.where(queue_name: name).each(&:discard)
+    end
+
+    def size
+      @size ||= ReadyExecution.where(queue_name: name).count
+    end
+
+    def ==(queue)
+      name == queue.name
+    end
+    alias_method :eql?, :==
+
+    def hash
+      name.hash
+    end
+  end
+end
