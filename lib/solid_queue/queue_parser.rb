26c6f7a2f84cff435392f52c27db0d541708a501
Rosa Gutierrez <rosa@37signals.com>
2023-09-13 12:21:08 +0200

    Allow configuring queues per worker using lists, wildcard and prefixes
    The following are accepted:
    - List of patterns or names separated by comma. Example: `background,default`.
    - `*`, matching all queues
    - Name prefixes. Example: `mailer*,production_*`.
    
    For now, the order is not taken into account, priorities need to be used for that,
    and there's no negation yet, but I'll add these two in the near future.

---
+++ lib/solid_queue/queue_parser.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module SolidQueue
+  class QueueParser
+    attr_reader :raw_queues, :relation
+
+    def initialize(queue_string, relation)
+      @raw_queues = queue_string.split(",").map(&:strip).presence || [ "*" ]
+      @relation = relation
+    end
+
+    def scoped_relation
+      if all? then relation.all
+      else
+        by_exact_names.or(by_prefixes)
+      end
+    end
+
+    private
+      def all?
+        "*".in? raw_queues
+      end
+
+      def by_exact_names
+        exact_names.any? ? relation.where(queue_name: exact_names) : relation.none
+      end
+
+      def by_prefixes
+        prefixes.any? ? relation.where(([ "queue_name LIKE ?" ] * prefixes.count).join(" OR "), *prefixes) : relation.none
+      end
+
+      def exact_names
+        @exact_names ||= raw_queues.select { |queue| !queue.include?("*") }
+      end
+
+      def prefixes
+        @prefixes ||= raw_queues.select { |queue| queue.ends_with?("*") }.map { |queue| queue.tr("*", "%") }
+      end
+  end
+end
