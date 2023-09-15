46f2d7eda5326551d44405b48a199125ce2157c4
Rosa Gutierrez <rosa@37signals.com>
2023-09-15 23:25:52 +0200

    Avoid replacing executions when moving jobs between states
    That is, when using `create_*_execution!` from job, if the job previously
    had a record for that, we'd create a new one and replace it. This causes
    problems with Mission Control when a thread dies taking care of a job and
    leaves it in a weird, in-between state.

--- app/models/solid_queue/job/executable.rb
+++ app/models/solid_queue/job/executable.rb
@@ -1,52 +1,54 @@
-module SolidQueue::Job::Executable
-  extend ActiveSupport::Concern
+module SolidQueue
+  module Job::Executable
+    extend ActiveSupport::Concern
 
-  included do
-    has_one :ready_execution, dependent: :destroy
-    has_one :claimed_execution, dependent: :destroy
-    has_one :failed_execution, dependent: :destroy
+    included do
+      has_one :ready_execution, dependent: :destroy
+      has_one :claimed_execution, dependent: :destroy
+      has_one :failed_execution, dependent: :destroy
 
-    has_one :scheduled_execution, dependent: :destroy
+      has_one :scheduled_execution, dependent: :destroy
 
-    after_create :prepare_for_execution
-  end
-
-  STATUSES = %w[ ready claimed failed scheduled ]
-
-  STATUSES.each do |status|
-    define_method("#{status}?") { public_send("#{status}_execution").present? }
-  end
-
-  def prepare_for_execution
-    if due?
-      create_ready_execution!
-    else
-      create_scheduled_execution!
+      after_create :prepare_for_execution
     end
-  end
 
-  def finished
-    touch(:finished_at)
-  end
+    STATUSES = %w[ ready claimed failed scheduled ]
 
-  def finished?
-    finished_at.present?
-  end
-
-  def failed_with(exception)
-    create_failed_execution!(exception: exception)
-  end
-
-  def discard
-    destroy unless claimed?
-  end
-
-  def retry
-    failed_execution&.retry
-  end
-
-  private
-    def due?
-      scheduled_at.nil? || scheduled_at <= Time.current
+    STATUSES.each do |status|
+      define_method("#{status}?") { public_send("#{status}_execution").present? }
     end
+
+    def prepare_for_execution
+      if due?
+        ReadyExecution.create_or_find_by!(job_id: id)
+      else
+        ScheduledExecution.create_or_find_by!(job_id: id)
+      end
+    end
+
+    def finished
+      touch(:finished_at)
+    end
+
+    def finished?
+      finished_at.present?
+    end
+
+    def failed_with(exception)
+      FailedExecution.create_or_find_by!(job_id: id, exception: exception)
+    end
+
+    def discard
+      destroy unless claimed?
+    end
+
+    def retry
+      failed_execution&.retry
+    end
+
+    private
+      def due?
+        scheduled_at.nil? || scheduled_at <= Time.current
+      end
+  end
 end
