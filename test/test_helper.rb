809eb4b231b1b0d99ead821144e8fabafa30bf39
Rosa Gutierrez <rosa@37signals.com>
2023-10-25 21:37:49 +0200

    Stop calling deprecated methods to set fixture paths in tests
    ```
    DEPRECATION WARNING: TestFixtures.fixture_path= is deprecated
    and will be removed in Rails 7.2. Use .fixture_paths= instead.
    (called from <top (required)> at
    /Users/rosa/Work/basecamp/solid_queue/test/test_helper.rb:13)
    DEPRECATION WARNING: TestFixtures.fixture_path is deprecated
    and will be removed in Rails 7.2. Use .fixture_paths instead.
    ```

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,9 @@ require "mocha/minitest"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_paths.first + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
 
