6050088d33e8a04b044417c0edaa8748a0a3899a
Rosa Gutierrez <rosa@37signals.com>
2023-11-05 13:12:58 +0100

    Refactor slightly how executions assume attributes from job
    Just to test how it feels to do it in this way.

--- app/models/solid_queue/blocked_execution.rb
+++ app/models/solid_queue/blocked_execution.rb
@@ -1,10 +1,3 @@
 class SolidQueue::BlockedExecution < SolidQueue::Execution
-  before_create :assume_attributes_from_job
-
-  private
-    def assume_attributes_from_job
-      super
-      self.concurrency_limit ||= job.concurrency_limit
-      self.concurrency_key   ||= job.concurrency_key
-    end
+  assume_attributes_from_job :concurrency_limit, :concurrency_key
 end
