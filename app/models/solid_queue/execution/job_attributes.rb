5ad79dc530e17cfe97f9de49d48f84053ba25126
Rosa Gutierrez <rosa@37signals.com>
2023-11-05 13:12:58 +0100

    Refactor slightly how executions assume attributes from job
    Just to test how it feels to do it in this way.

---
+++ app/models/solid_queue/execution/job_attributes.rb
@@ -0,0 +1,22 @@
+module SolidQueue
+  class Execution
+    module JobAttributes
+      extend ActiveSupport::Concern
+
+      ASSUMIBLE_ATTRIBUTES_FROM_JOB = %i[ queue_name priority ]
+
+      class_methods do
+        def assume_attributes_from_job(*attributes)
+          before_create -> { assume_attributes_from_job(ASSUMIBLE_ATTRIBUTES_FROM_JOB | attributes) }
+        end
+      end
+
+      private
+        def assume_attributes_from_job(attributes)
+          attributes.each do |attribute|
+            send("#{attribute}=", job.send(attribute))
+          end
+        end
+    end
+  end
+end
