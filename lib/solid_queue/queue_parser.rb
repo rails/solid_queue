cd72097d76f023da3457ced5ab526c84efd26f59
Rosa Gutierrez <rosa@37signals.com>
2023-11-20 21:40:01 +0100

    Remove support for prefix matching in queues assigned to a worker
    This would generate queries like
    ```
      WHERE `solid_queue_ready_executions.queue_name` LIKE 'prefix%'
    ```
    that aren't performant enough due to the sorting by priority, so we've
    decided not to support these for now.
    
    Thanks to @djmb for the assistance with this.

--- lib/solid_queue/queue_parser.rb
+++ lib/solid_queue/queue_parser.rb
@@ -12,7 +12,7 @@ module SolidQueue
     def scoped_relation
       if all? then relation.all
       else
-        by_exact_names.or(by_prefixes)
+        by_exact_names
       end
     end
 
@@ -25,16 +25,8 @@ module SolidQueue
         exact_names.any? ? relation.where(queue_name: exact_names) : relation.none
       end
 
-      def by_prefixes
-        prefixes.any? ? relation.where(([ "queue_name LIKE ?" ] * prefixes.count).join(" OR "), *prefixes) : relation.none
-      end
-
       def exact_names
         @exact_names ||= raw_queues.select { |queue| !queue.include?("*") }
-      end
-
-      def prefixes
-        @prefixes ||= raw_queues.select { |queue| queue.ends_with?("*") }.map { |queue| queue.tr("*", "%") }
       end
   end
 end
