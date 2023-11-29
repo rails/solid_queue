808d9fb3d808b15add3dde0b6b67a7b34f2120af
Rosa Gutierrez <rosa@37signals.com>
2023-11-29 22:34:44 +0100

    Reorder scheduled execution code a bit

--- app/models/solid_queue/scheduled_execution.rb
+++ app/models/solid_queue/scheduled_execution.rb
@@ -15,18 +15,26 @@ class SolidQueue::ScheduledExecution < SolidQueue::Execution
 
     private
       def prepare_batch(batch)
+        if batch.empty? then []
+        else
+          promote_batch_to_ready(batch)
+        end
+      end
+
+      def promote_batch_to_ready(batch)
+        rows = ready_rows_from_batch(batch)
+
+        SolidQueue::ReadyExecution.insert_all(rows)
+        SolidQueue::ReadyExecution.where(job_id: batch.map(&:job_id)).pluck(:job_id).tap do |enqueued_job_ids|
+          where(job_id: enqueued_job_ids).delete_all
+        end
+      end
+
+      def ready_rows_from_batch(batch)
         prepared_at = Time.current
 
-        rows = batch.map do |scheduled_execution|
+        batch.map do |scheduled_execution|
           scheduled_execution.ready_attributes.merge(created_at: prepared_at)
-        end
-
-        if rows.empty? then []
-        else
-          SolidQueue::ReadyExecution.insert_all(rows)
-          SolidQueue::ReadyExecution.where(job_id: batch.map(&:job_id)).pluck(:job_id).tap do |enqueued_job_ids|
-            where(job_id: enqueued_job_ids).delete_all
-          end
         end
       end
   end
