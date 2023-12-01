047270c16a2c02479f63536697ec7cd17feb1426
Rosa Gutierrez <rosa@37signals.com>
2023-12-01 17:00:53 +0100

    Bring back support for prefix matching in queues assigned to a worker
    This was removed in https://github.com/basecamp/solid_queue/pull/45, as the
    resulting queries for polling using `queue_name LIKE 'something%'` were very
    slow as they prevented MySQL from using an index for ordering. The idea after
    removing that was to support namespaces so we could combine a namespace with
    a "*" for queue names, to have support for a single prefix. However, namespaces
    felt a too big of a change just to accomodate for our staging and beta setups,
    so I've brought this back, but this time with a different implementation,
    based on fetching the list of queue names relevant for polling, filtering there
    with the prefix. This query is very fast because it can use an existing index
    starting with queue_name and apply the Loose Index Scan technique to examine
    as many rows as distinct values there are.

--- app/models/solid_queue/pause.rb
+++ app/models/solid_queue/pause.rb
@@ -1,7 +1,4 @@
 module SolidQueue
   class Pause < Record
-    def self.all_queue_names
-      all.pluck(:queue_name)
-    end
   end
 end
--- lib/solid_queue/queue_selector.rb
+++ lib/solid_queue/queue_selector.rb
@@ -20,7 +20,7 @@ module SolidQueue
       def queue_names
         if all? then filter_paused_queues
         else
-          filter_paused_queues(exact_names)
+          filter_paused_queues(exact_names + prefixed_names)
         end
       end
 
@@ -29,16 +29,32 @@ module SolidQueue
       end
 
       def filter_paused_queues(queues = [])
-        paused_queues = Pause.all_queue_names
+        paused_queues = Pause.all.pluck(:queue_name)
+
         if paused_queues.empty? then queues
         else
-          queues = queues.presence || Queue.all.map(&:name)
+          queues = queues.presence || all_queue_names
           queues - paused_queues
         end
       end
 
       def exact_names
         @exact_names ||= raw_queues.select { |queue| !queue.include?("*") }
+      end
+
+      def prefixed_names
+        if prefixes.empty? then []
+        else
+          relation.where(([ "queue_name LIKE ?" ] * prefixes.count).join(" OR "), *prefixes).distinct(:queue_name).pluck(:queue_name)
+        end
+      end
+
+      def prefixes
+        @prefixes ||= raw_queues.select { |queue| queue.ends_with?("*") }.map { |queue| queue.tr("*", "%") }
+      end
+
+      def all_queue_names
+        relation.distinct(:queue_name).pluck(:queue_name)
       end
   end
 end
