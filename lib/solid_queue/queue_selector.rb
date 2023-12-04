1a0913f035efc244b067f0494d870c9bd3f8f191
Rosa Gutierrez <rosa@37signals.com>
2023-12-04 20:19:33 +0100

    Make sure all queues are included in the selection only when "*" is specified
    Before, if no queue_name was found matching the given prefix, we'd just select
    all queues.

--- lib/solid_queue/queue_selector.rb
+++ lib/solid_queue/queue_selector.rb
@@ -10,36 +10,40 @@ module SolidQueue
     end
 
     def scoped_relations
-      if queue_names.empty? then [ relation.all ]
+      case
+      when all?  then [ relation.all ]
+      when none? then [ relation.none ]
       else
         queue_names.map { |queue_name| relation.queued_as(queue_name) }
       end
     end
 
     private
+      def all?
+        include_all_queues? && paused_queues.empty?
+      end
+
+      def none?
+        queue_names.empty?
+      end
+
       def queue_names
-        if all? then filter_paused_queues
+        @queue_names ||= eligible_queues - paused_queues
+      end
+
+      def eligible_queues
+        if include_all_queues? then all_queues
         else
-          filter_paused_queues(exact_names + prefixed_names)
+          exact_names + prefixed_names
         end
       end
 
-      def all?
+      def include_all_queues?
         "*".in? raw_queues
       end
 
-      def filter_paused_queues(queues = [])
-        paused_queues = Pause.all.pluck(:queue_name)
-
-        if paused_queues.empty? then queues
-        else
-          queues = queues.presence || all_queue_names
-          queues - paused_queues
-        end
-      end
-
       def exact_names
-        @exact_names ||= raw_queues.select { |queue| !queue.include?("*") }
+        raw_queues.select { |queue| !queue.include?("*") }
       end
 
       def prefixed_names
@@ -53,8 +57,12 @@ module SolidQueue
         @prefixes ||= raw_queues.select { |queue| queue.ends_with?("*") }.map { |queue| queue.tr("*", "%") }
       end
 
-      def all_queue_names
+      def all_queues
         relation.distinct(:queue_name).pluck(:queue_name)
+      end
+
+      def paused_queues
+        @paused_queues ||= Pause.all.pluck(:queue_name)
       end
   end
 end
