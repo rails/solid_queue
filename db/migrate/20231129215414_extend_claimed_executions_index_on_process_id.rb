9d5ff3eb933f475fd49a3321d8396ce028911ca7
Rosa Gutierrez <rosa@37signals.com>
2023-12-12 21:24:31 +0100

    Unify migrations into just one
    Cleaning this up a bit before public release, and using the chance of having
    to move Solid Queue to its own DB.

--- db/migrate/20231129215414_extend_claimed_executions_index_on_process_id.rb
+++
@@ -1,6 +0,0 @@
-class ExtendClaimedExecutionsIndexOnProcessId < ActiveRecord::Migration[7.1]
-  def change
-    add_index :solid_queue_claimed_executions, [ :process_id, :job_id ]
-    remove_index :solid_queue_claimed_executions, :process_id
-  end
-end
--- db/migrate/20231211200639_add_new_attributes_to_processes.rb
+++
@@ -1,9 +0,0 @@
-class AddNewAttributesToProcesses < ActiveRecord::Migration[7.1]
-  def change
-    change_table :solid_queue_processes do |t|
-      t.string :kind, null: false
-      t.string :hostname
-      t.integer :pid, null: false
-    end
-  end
-end
