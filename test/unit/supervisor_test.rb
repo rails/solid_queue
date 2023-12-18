879a56d98a2b488b464a69c6028d9bb4a6526aad
Rosa Gutierrez <rosa@37signals.com>
2023-12-18 12:29:54 +0100

    Fix wrong calls to wait for registered processes in tests
    These were working most of the time by chance, since we'd just wait for the
    default 1 second timeout and the processes would be there, but sometimes in CI,
    this wouldn't be the case.

--- test/unit/supervisor_test.rb
+++ test/unit/supervisor_test.rb
@@ -18,7 +18,7 @@ class SupervisorTest < ActiveSupport::TestCase
 
   test "start in work mode (default)" do
     pid = run_supervisor_as_fork
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     assert_registered_supervisor(pid)
     assert_registered_workers(2, supervisor_pid: pid)
@@ -30,7 +30,7 @@ class SupervisorTest < ActiveSupport::TestCase
 
   test "start in dispatch mode" do
     pid = run_supervisor_as_fork(mode: :dispatch)
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     assert_registered_supervisor(pid)
     assert_registered_dispatcher(supervisor_pid: pid)
@@ -44,7 +44,7 @@ class SupervisorTest < ActiveSupport::TestCase
     assert_not File.exist?(@pidfile)
 
     pid = run_supervisor_as_fork(mode: :all)
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     assert File.exist?(@pidfile)
     assert_equal pid, File.read(@pidfile).strip.to_i
@@ -59,7 +59,7 @@ class SupervisorTest < ActiveSupport::TestCase
     File.write(@pidfile, ::Process.pid.to_s)
 
     pid = run_supervisor_as_fork(mode: :all)
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     assert File.exist?(@pidfile)
     assert_not_equal pid, File.read(@pidfile).strip.to_i
@@ -69,15 +69,17 @@ class SupervisorTest < ActiveSupport::TestCase
 
   test "deletes previous pidfile if the owner is dead" do
     pid = run_supervisor_as_fork(mode: :all)
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     terminate_process(pid, signal: :KILL)
 
     assert File.exist?(@pidfile)
     assert_equal pid, File.read(@pidfile).strip.to_i
 
+    wait_for_registered_processes(0)
+
     pid = run_supervisor_as_fork(mode: :all)
-    wait_for_registered_processes(0.3)
+    wait_for_registered_processes(4)
 
     assert File.exist?(@pidfile)
     assert_equal pid, File.read(@pidfile).strip.to_i
