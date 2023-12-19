20bb987670632505c5b2cda919f3608c76f623ea
Chris Oliver <excid3@gmail.com>
2023-12-19 17:12:12 -0600

    Copy over an example config on install

--- lib/generators/solid_queue/install/install_generator.rb
+++ lib/generators/solid_queue/install/install_generator.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SolidQueue::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path("templates", __dir__)
+
   class_option :skip_migrations, type: :boolean, default: nil, desc: "Skip migrations"
 
   def add_solid_queue
@@ -9,6 +11,8 @@ class SolidQueue::InstallGenerator < Rails::Generators::Base
         gsub_file env_config, /(# )?config\.active_job\.queue_adapter\s+=.*/, "config.active_job.queue_adapter = :solid_queue"
       end
     end
+
+    copy_file "config.yml", "config/solid_queue.yml"
   end
 
   def create_migrations
