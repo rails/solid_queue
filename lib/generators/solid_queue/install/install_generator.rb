a58d5a7274d7302635dea7ae38399130050399f8
Rosa Gutierrez <rosa@37signals.com>
2023-12-26 21:08:45 +0100

    Set solid_queue as AJ adapter only in production via generator
    It doesn't quite work correclty yet in test or dev environments.

--- lib/generators/solid_queue/install/install_generator.rb
+++ lib/generators/solid_queue/install/install_generator.rb
@@ -6,10 +6,8 @@ class SolidQueue::InstallGenerator < Rails::Generators::Base
   class_option :skip_migrations, type: :boolean, default: nil, desc: "Skip migrations"
 
   def add_solid_queue
-    %w[ development test production ].each do |env_name|
-      if (env_config = Pathname(destination_root).join("config/environments/#{env_name}.rb")).exist?
-        gsub_file env_config, /(# )?config\.active_job\.queue_adapter\s+=.*/, "config.active_job.queue_adapter = :solid_queue"
-      end
+    if (env_config = Pathname(destination_root).join("config/environments/production.rb")).exist?
+      gsub_file env_config, /(# )?config\.active_job\.queue_adapter\s+=.*/, "config.active_job.queue_adapter = :solid_queue"
     end
 
     copy_file "config.yml", "config/solid_queue.yml"
