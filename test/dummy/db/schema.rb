48ba9fbe65fe14c1daadadece5498cabfbcf4778
Rosa Gutierrez <rosa@37signals.com>
2024-01-10 15:37:10 +0100

    Add missing index for release to blocked_executions table
    I inadvertently lost this one on #57.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_11_200639) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_143450) do
   create_table "job_results", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "queue_name"
     t.string "status"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_11_200639) do
     t.string "concurrency_key", null: false
     t.datetime "expires_at", null: false
     t.datetime "created_at", null: false
+    t.index ["concurrency_key", "priority", "job_id"], name: "index_solid_queue_blocked_executions_for_release"
     t.index ["expires_at", "concurrency_key"], name: "index_solid_queue_blocked_executions_for_maintenance"
     t.index ["job_id"], name: "index_solid_queue_blocked_executions_on_job_id", unique: true
   end
