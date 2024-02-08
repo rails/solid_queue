b50372e8a17544b9373b883f957a22996298692f
JP Camara <48120+jpcamara@users.noreply.github.com>
2024-02-07 22:16:28 -0500

    Make sure `batch` is still first arg of the batch callback
    * Add spec for adding arguments and options to the batch callback

--- app/models/solid_queue/job_batch.rb
+++ app/models/solid_queue/job_batch.rb
@@ -82,6 +82,7 @@ module SolidQueue
       if on_finish_active_job.present?
         active_job = ActiveJob::Base.deserialize(on_finish_active_job)
         active_job.send(:deserialize_arguments_if_needed)
+        active_job.arguments = [self] + Array.wrap(active_job.arguments)
         ActiveJob.perform_all_later([active_job])
         attrs[:job] = Job.find_by(active_job_id: active_job.job_id)
       end
