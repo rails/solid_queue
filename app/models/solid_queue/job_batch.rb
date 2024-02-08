cd7a62f7fee93f3ae540b943a215fb5273252eec
JP Camara <48120+jpcamara@users.noreply.github.com>
2024-02-07 22:06:21 -0500

    Allow batch jobs to be instances
    * This means we can store the arguments and settings by letting the user do `BatchJob.new(arguments).set(options)`
    
    * Yield the batch in `enqueue` in case someone needs info from it
    
    * When you serialize then deserialize an activejob instance, the arguments are in the serialized_arguments field and can only be transferred over by the private method `deserialize_arguments_if_needed`. This is pretty janky, so there is probably something i'm missing
    
    * `perform_all_later` let's us do a perform_later even with instance, which does not seem to be possible on the instances themselves

--- app/models/solid_queue/job_batch.rb
+++ app/models/solid_queue/job_batch.rb
@@ -5,6 +5,9 @@ module SolidQueue
     belongs_to :job, foreign_key: :job_id, optional: true
     has_many :jobs, foreign_key: :batch_id
 
+    serialize :on_finish_active_job, coder: JSON
+    serialize :on_success_active_job, coder: JSON
+
     scope :incomplete, -> {
       where(finished_at: nil).where("changed_at IS NOT NULL OR last_changed_at < ?", 1.hour.ago)
     }
@@ -21,7 +24,7 @@ module SolidQueue
         transaction do
           job_batch = create!(batch_attributes(attributes))
           ActiveSupport::IsolatedExecutionState[:current_batch_id] = job_batch.id
-          yield
+          yield job_batch
         end
 
         job_batch
@@ -40,20 +43,22 @@ module SolidQueue
       private
 
         def batch_attributes(attributes)
-          attributes = case attributes
-          in { on_finish: on_finish_klass }
-            attributes.merge(
-              job_class: on_finish_klass,
-              completion_type: "success"
-            )
-          in { on_success: on_success_klass }
-            attributes.merge(
-              job_class: on_success_klass,
-              completion_type: "success"
-            )
+          on_finish_klass = attributes.delete(:on_finish)
+          on_success_klass = attributes.delete(:on_success)
+
+          if on_finish_klass.present?
+            attributes[:on_finish_active_job] = as_active_job(on_finish_klass).serialize
           end
 
-          attributes.except(:on_finish, :on_success)
+          if on_success_klass.present?
+            attributes[:on_success_active_job] = as_active_job(on_success_klass).serialize
+          end
+
+          attributes
+        end
+
+        def as_active_job(active_job_klass)
+          active_job_klass.is_a?(ActiveJob::Base) ? active_job_klass : active_job_klass.new
         end
     end
 
@@ -74,9 +79,10 @@ module SolidQueue
 
       attrs = {}
 
-      if job_class.present?
-        job_klass = job_class.constantize
-        active_job = job_klass.perform_later(self)
+      if on_finish_active_job.present?
+        active_job = ActiveJob::Base.deserialize(on_finish_active_job)
+        active_job.send(:deserialize_arguments_if_needed)
+        ActiveJob.perform_all_later([active_job])
         attrs[:job] = Job.find_by(active_job_id: active_job.job_id)
       end
 
