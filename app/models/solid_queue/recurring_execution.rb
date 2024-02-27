1043984066b1bc3b733756f105091cc0e26913ed
Rosa Gutierrez <rosa@37signals.com>
2024-02-27 18:19:07 +0100

    Add a clear_in_batches method to delete recurring executions
    This is useful for those who decide not to have FKs that ensure recurring
    executions are deleted when jobs are cleared up, so they can just
    call this method periodically to clear orphaned executions.

--- app/models/solid_queue/recurring_execution.rb
+++ app/models/solid_queue/recurring_execution.rb
@@ -2,14 +2,25 @@
 
 module SolidQueue
   class RecurringExecution < Execution
-    def self.record(task_key, run_at, &block)
-      transaction do
-        if job_id = block.call
-          create!(job_id: job_id, task_key: task_key, run_at: run_at)
+    scope :clearable, -> { where.missing(:job) }
+
+    class << self
+      def record(task_key, run_at, &block)
+        transaction do
+          if job_id = block.call
+            create!(job_id: job_id, task_key: task_key, run_at: run_at)
+          end
+        end
+      rescue ActiveRecord::RecordNotUnique
+        SolidQueue.logger.info("[SolidQueue] Skipped recurring task #{task_key} at #{run_at} — already dispatched")
+      end
+
+      def clear_in_batches(batch_size: 500)
+        loop do
+          records_deleted = clearable.limit(batch_size).delete_all
+          break if records_deleted == 0
         end
       end
-    rescue ActiveRecord::RecordNotUnique
-      SolidQueue.logger.info("[SolidQueue] Skipped recurring task #{task_key} at #{run_at} — already dispatched")
     end
   end
 end
