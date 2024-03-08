0fb39bfc8cc94b779d852d03c7984c8afb1ff293
Dorian Marié <dorian@dorianmarie.com>
2024-03-08 07:38:13 +0100

    WIP upgrade rails to main and gems, and simpler config/database.yml

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_143450) do
+ActiveRecord::Schema[7.2].define(version: 2024_01_10_143450) do
   create_table "job_results", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "queue_name"
     t.string "status"
