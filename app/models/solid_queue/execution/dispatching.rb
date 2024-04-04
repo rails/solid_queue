e17ef7cb374c2172aa4e4cc7865c5c29126be020
Rosa Gutierrez <rosa@37signals.com>
2024-04-04 14:07:41 +0200

    Try another workaround to avoid deadlock when dispatching scheduled jobs
    Instead of deleting these using the index on `job_id`, do the DELETE relying on the
    primary key.

--- app/models/solid_queue/execution/dispatching.rb
+++ app/models/solid_queue/execution/dispatching.rb
@@ -10,10 +10,7 @@ module SolidQueue
           jobs = Job.where(id: job_ids)
 
           Job.dispatch_all(jobs).map(&:id).then do |dispatched_job_ids|
-            if dispatched_job_ids.none? then 0
-            else
-              where(job_id: dispatched_job_ids).order(:job_id).delete_all
-            end
+            where(id: where(job_id: dispatched_job_ids).pluck(:id)).delete_all
           end
         end
       end
