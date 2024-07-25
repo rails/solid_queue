eb026d34dce2c7b1945ed80a14ebb368140dc4db
Rosa Gutierrez <rosa@37signals.com>
2024-07-25 17:34:06 +0200

    Release orphaned executions just once, before booting the supervisor
    Instead of periodically.

--- lib/solid_queue/supervisor/maintenance.rb
+++ lib/solid_queue/supervisor/maintenance.rb
@@ -1,30 +1,34 @@
 module SolidQueue
   module Supervisor::Maintenance
+    extend ActiveSupport::Concern
+
+    included do
+      after_boot :release_orphaned_executions
+    end
+
     private
+      def launch_maintenance_task
+        @maintenance_task = Concurrent::TimerTask.new(run_now: true, execution_interval: SolidQueue.process_alive_threshold) do
+          prune_dead_processes
+        end
 
-    def launch_maintenance_task
-      @maintenance_task = Concurrent::TimerTask.new(run_now: true, execution_interval: SolidQueue.process_alive_threshold) do
-        prune_dead_processes
-        release_orphaned_executions
+        @maintenance_task.add_observer do |_, _, error|
+          handle_thread_error(error) if error
+        end
+
+        @maintenance_task.execute
       end
 
-      @maintenance_task.add_observer do |_, _, error|
-        handle_thread_error(error) if error
+      def stop_maintenance_task
+        @maintenance_task&.shutdown
       end
 
-      @maintenance_task.execute
-    end
+      def prune_dead_processes
+        wrap_in_app_executor { SolidQueue::Process.prune }
+      end
 
-    def stop_maintenance_task
-      @maintenance_task&.shutdown
-    end
-
-    def prune_dead_processes
-      wrap_in_app_executor { SolidQueue::Process.prune }
-    end
-
-    def release_orphaned_executions
-      wrap_in_app_executor { SolidQueue::ClaimedExecution.orphaned.release_all }
-    end
+      def release_orphaned_executions
+        wrap_in_app_executor { SolidQueue::ClaimedExecution.orphaned.release_all }
+      end
   end
 end
