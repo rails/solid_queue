845d36ee58bec5dbc06b943064c92a2bb97628a1
Rosa Gutierrez <rosa@37signals.com>
2024-08-05 16:12:29 +0200

    Handle concurrent inserts in PostgreSQL when enqueuing recurring tasks
    Very similar to inserting recurring tasks on boot.
    
    PostgreSQL fails and aborts the current transaction when it hits
    a duplicate key conflict during two concurrent INSERTs for the same
    value of an unique index. We need to use `insert` instead of `create`
    here and indicate unique_by to ignore duplicate rows by this value
    when inserting

--- app/models/solid_queue/recurring_execution.rb
+++ app/models/solid_queue/recurring_execution.rb
@@ -7,16 +7,29 @@ module SolidQueue
     scope :clearable, -> { where.missing(:job) }
 
     class << self
+      def create_or_insert!(**attributes)
+        if connection.supports_insert_conflict_target?
+          # PostgreSQL fails and aborts the current transaction when it hits a duplicate key conflict
+          # during two concurrent INSERTs for the same value of an unique index. We need to explicitly
+          # indicate unique_by to ignore duplicate rows by this value when inserting
+          unless insert(attributes, unique_by: [ :task_key, :run_at ]).any?
+            raise AlreadyRecorded
+          end
+        else
+          create!(**attributes)
+        end
+      rescue ActiveRecord::RecordNotUnique
+        raise AlreadyRecorded
+      end
+
       def record(task_key, run_at, &block)
         transaction do
           block.call.tap do |active_job|
             if active_job
-              create!(job_id: active_job.provider_job_id, task_key: task_key, run_at: run_at)
+              create_or_insert!(job_id: active_job.provider_job_id, task_key: task_key, run_at: run_at)
             end
           end
         end
-      rescue ActiveRecord::RecordNotUnique => e
-        raise AlreadyRecorded
       end
 
       def clear_in_batches(batch_size: 500)
