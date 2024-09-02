18dd153ce3fa49f0b7045c7c7cb8e2587c074f1e
Rosa Gutierrez <rosa@37signals.com>
2024-09-02 13:24:58 +0200

    Improve message for ProcessExitError exception
    To show as much information as possible.

--- lib/solid_queue/processes/process_exit_error.rb
+++ lib/solid_queue/processes/process_exit_error.rb
@@ -4,10 +4,13 @@ module SolidQueue
   module Processes
     class ProcessExitError < RuntimeError
       def initialize(status)
-        message = case
-        when status.exitstatus.present? then "Process pid=#{status.pid} exited with status #{status.  exitstatus}"
-        when status.signaled? then "Process pid=#{status.pid} received unhandled signal #{status. termsig}"
-        else "Process pid=#{status.pid} exited unexpectedly"
+        message = "Process pid=#{status.pid} exited unexpectedly."
+        if status.exitstatus.present?
+          message += " Exited with status #{status.exitstatus}."
+        end
+
+        if status.signaled?
+          message += " Received unhandled signal #{status.termsig}."
         end
 
         super(message)
