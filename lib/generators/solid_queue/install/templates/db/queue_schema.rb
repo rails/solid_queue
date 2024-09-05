5f75a72f449705ffcecec57e15b646d5c6dca21b
Rosa Gutierrez <rosa@37signals.com>
2024-09-05 19:02:32 +0200

    Go back to version 7.1 for the queue schema
    This is the minimum version we support, and otherwise apps using
    Rails < 8 won't be able to install Solid Queue properly. Thanks a lot to
    @fractaledmind for catching this in #323.

--- lib/generators/solid_queue/install/templates/db/queue_schema.rb
+++ lib/generators/solid_queue/install/templates/db/queue_schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema[8.0].define(version: 2024_09_04_193154) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_04_193154) do
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.integer "job_id", null: false
     t.string "queue_name", null: false
