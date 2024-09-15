06424fa86e3f031e5652b7acd5c29d32e18c7ec8
Ugurcan Kaya <uurcan@pasilobus.com>
2024-09-15 01:42:40 -0700

    refactor adapter configuration in generator

--- lib/generators/solid_queue/install/install_generator.rb
+++ lib/generators/solid_queue/install/install_generator.rb
@@ -12,9 +12,14 @@ class SolidQueue::InstallGenerator < Rails::Generators::Base
   end
 
   def configure_active_job_adapter
-    gsub_file Pathname(destination_root).join("config/environments/production.rb"),
-      /(# )?config\.active_job\.queue_adapter\s+=.*/,
-      "config.active_job.queue_adapter = :solid_queue\n" +
-      "  config.solid_queue.connects_to = { database: { writing: :queue } }\n"
+    production_rb = Pathname(destination_root).join("config/environments/production.rb")
+
+    # Replace the active_job queue_adapter line
+    gsub_file(production_rb, /(# )?config\.active_job\.queue_adapter\s+=.*/, "config.active_job.queue_adapter = :solid_queue")
+
+    # Add the solid_queue connects_to line if it doesn't exist
+    unless File.foreach(production_rb).any? { |line| line.include?("config.solid_queue.connects_to = { database: { writing: :queue } }") }
+      inject_into_file(production_rb, "\n  config.solid_queue.connects_to = { database: { writing: :queue } }", after: "config.active_job.queue_adapter = :solid_queue\n")
+    end
   end
 end
