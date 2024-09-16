c9e234d1948e572447c45947046e49c3ab674b51
Ugurcan Kaya <uurcan@pasilobus.com>
2024-09-16 16:02:38 -0700

    refactor to use regex to check for solid_queue connects_to

--- lib/generators/solid_queue/install/install_generator.rb
+++ lib/generators/solid_queue/install/install_generator.rb
@@ -14,12 +14,19 @@ class SolidQueue::InstallGenerator < Rails::Generators::Base
   def configure_active_job_adapter
     production_rb = Pathname(destination_root).join("config/environments/production.rb")
 
-    # Replace the active_job queue_adapter line
-    gsub_file(production_rb, /(# )?config\.active_job\.queue_adapter\s+=.*/, "config.active_job.queue_adapter = :solid_queue")
+    # Replace or set `config.active_job.queue_adapter`
+    gsub_file Pathname(destination_root).join("config/environments/production.rb"),
+    /config\.active_job\.queue_adapter\s+=.*/,
+    "config.active_job.queue_adapter = :solid_queue"
 
-    # Add the solid_queue connects_to line if it doesn't exist
-    unless File.foreach(production_rb).any? { |line| line.include?("config.solid_queue.connects_to = { database: { writing: :queue } }") }
-      inject_into_file(production_rb, "\n  config.solid_queue.connects_to = { database: { writing: :queue } }", after: "config.active_job.queue_adapter = :solid_queue\n")
-    end
+    # Inject `config.solid_queue.connects_to` if not already present
+    gsub_file Pathname(destination_root).join("config/environments/production.rb"),
+    /^\s*config\.solid_queue\.connects_to\s+=\s+\{.*\}\n/,
+    '',
+    verbose: false # If found, do nothing
+
+    inject_into_file Pathname(destination_root).join("config/environments/production.rb"),
+    "\n  config.solid_queue.connects_to = { database: { writing: :queue } }",
+    after: /config\.active_job\.queue_adapter\s+=.*/
   end
 end
