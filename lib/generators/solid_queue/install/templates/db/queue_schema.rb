eeb0d47471723ec6e093bc9e7e001839258fca4e
Rosa Gutierrez <rosa@37signals.com>
2024-09-22 12:15:06 +0200

    Use version 1 for schema, to match Solid Cache's
    h/t to @brunoprietog for catching this.

--- lib/generators/solid_queue/install/templates/db/queue_schema.rb
+++ lib/generators/solid_queue/install/templates/db/queue_schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema[7.1].define(version: 2024_09_04_193154) do
+ActiveRecord::Schema[7.1].define(version: 1) do
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.bigint "job_id", null: false
     t.string "queue_name", null: false
