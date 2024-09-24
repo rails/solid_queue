5bffda3512ca0b2660569b529822d44f7f9cd064
JP Camara <48120+jpcamara@users.noreply.github.com>
2024-09-23 22:50:29 -0400

    Handle on_failure and on_success
    * on_failure fires the first time any of the jobs fail, even once
    
    * on_success only fires if all jobs work (after retries)
    
    * remove unused job_id

--- db/migrate/20240131013203_create_solid_queue_batch_table.rb
+++ db/migrate/20240131013203_create_solid_queue_batch_table.rb
@@ -1,9 +1,9 @@
 class CreateSolidQueueBatchTable < ActiveRecord::Migration[7.1]
   def change
     create_table :solid_queue_job_batches do |t|
-      t.references :job, index: { unique: true }
       t.text :on_finish_active_job
       t.text :on_success_active_job
+      t.text :on_failure_active_job
       t.datetime :finished_at
       t.datetime :changed_at
       t.datetime :last_changed_at
@@ -16,6 +16,5 @@ class CreateSolidQueueBatchTable < ActiveRecord::Migration[7.1]
 
     add_reference :solid_queue_jobs, :batch, index: true
     add_foreign_key :solid_queue_jobs, :solid_queue_job_batches, column: :batch_id, on_delete: :cascade
-    add_foreign_key :solid_queue_job_batches, :solid_queue_jobs, column: :job_id
   end
 end
