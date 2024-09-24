22402f847a2cffea494ad25bf6068dda745fa095
JP Camara <48120+jpcamara@users.noreply.github.com>
2024-09-23 22:50:29 -0400

    Handle on_failure and on_success
    * on_failure fires the first time any of the jobs fail, even once
    
    * on_success only fires if all jobs work (after retries)
    
    * remove unused job_id

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_19_165045) do
   end
 
   create_table "solid_queue_job_batches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "job_id"
     t.text "on_finish_active_job"
     t.text "on_success_active_job"
+    t.text "on_failure_active_job"
     t.datetime "finished_at"
     t.datetime "changed_at"
     t.datetime "last_changed_at"
@@ -57,7 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_19_165045) do
     t.datetime "updated_at", null: false
     t.index ["changed_at"], name: "index_solid_queue_job_batches_on_changed_at"
     t.index ["finished_at"], name: "index_solid_queue_job_batches_on_finished_at"
-    t.index ["job_id"], name: "index_solid_queue_job_batches_on_job_id", unique: true
     t.index ["last_changed_at"], name: "index_solid_queue_job_batches_on_last_changed_at"
   end
 
@@ -160,7 +159,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_19_165045) do
   add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_claimed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_failed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_job_batches", "solid_queue_jobs", column: "job_id"
   add_foreign_key "solid_queue_jobs", "solid_queue_job_batches", column: "batch_id", on_delete: :cascade
   add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
