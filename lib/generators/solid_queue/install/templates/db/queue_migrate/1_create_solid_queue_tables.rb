d9f124a87e01e96fdbdc4b40fb78459cbc9598e1
Matt Almeida <matt7@hey.com>
2024-10-13 18:01:53 -0400

    Update 1_create_solid_queue_tables.rb

--- lib/generators/solid_queue/install/templates/db/queue_migrate/1_create_solid_queue_tables.rb
+++ lib/generators/solid_queue/install/templates/db/queue_migrate/1_create_solid_queue_tables.rb
@@ -1,5 +1,5 @@
 class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
-  create_table "solid_queue_blocked_executions", if_not_exists: true do |t|
+  create_table "solid_queue_blocked_executions" do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.string "queue_name", null: false
     t.integer "priority", default: 0, null: false
@@ -10,20 +10,20 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "expires_at", "concurrency_key" ], name: "index_solid_queue_blocked_executions_for_maintenance"
   end
 
-  create_table "solid_queue_claimed_executions", if_not_exists: true do |t|
+  create_table "solid_queue_claimed_executions" do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.references :process, index: false
     t.datetime "created_at", null: false
     t.index [ "process_id", "job_id" ], name: "index_solid_queue_claimed_executions_on_process_id_and_job_id"
   end
 
-  create_table "solid_queue_failed_executions", if_not_exists: true  do |t|
+  create_table "solid_queue_failed_executions"  do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.text "error"
     t.datetime "created_at", null: false
   end
 
-  create_table "solid_queue_jobs", if_not_exists: true do |t|
+  create_table "solid_queue_jobs" do |t|
     t.string "queue_name", null: false
     t.string "class_name", null: false, index: true
     t.text "arguments"
@@ -38,12 +38,12 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "scheduled_at", "finished_at" ], name: "index_solid_queue_jobs_for_alerting"
   end
 
-  create_table "solid_queue_pauses", if_not_exists: true do |t|
+  create_table "solid_queue_pauses" do |t|
     t.string "queue_name", null: false, index: { unique: true }
     t.datetime "created_at", null: false
   end
 
-  create_table "solid_queue_processes", if_not_exists: true do |t|
+  create_table "solid_queue_processes" do |t|
     t.string "kind", null: false
     t.datetime "last_heartbeat_at", null: false, index: true
     t.references :supervisor
@@ -55,7 +55,7 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "name", "supervisor_id" ], name: "index_solid_queue_processes_on_name_and_supervisor_id", unique: true
   end
 
-  create_table "solid_queue_ready_executions", if_not_exists: true do |t|
+  create_table "solid_queue_ready_executions" do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.string "queue_name", null: false
     t.integer "priority", default: 0, null: false
@@ -64,7 +64,7 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "queue_name", "priority", "job_id" ], name: "index_solid_queue_poll_by_queue"
   end
 
-  create_table "solid_queue_recurring_executions", if_not_exists: true do |t|
+  create_table "solid_queue_recurring_executions" do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.string "task_key", null: false
     t.datetime "run_at", null: false
@@ -72,7 +72,7 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "task_key", "run_at" ], name: "index_solid_queue_recurring_executions_on_task_key_and_run_at", unique: true
   end
 
-  create_table "solid_queue_recurring_tasks", if_not_exists: true do |t|
+  create_table "solid_queue_recurring_tasks" do |t|
     t.string "key", null: false, index: { unique: true }
     t.string "schedule", null: false
     t.string "command", limit: 2048
@@ -86,7 +86,7 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.datetime "updated_at", null: false
   end
 
-  create_table "solid_queue_scheduled_executions", if_not_exists: true do |t|
+  create_table "solid_queue_scheduled_executions" do |t|
     t.references :job, null: false, index: { unique: true }, foreign_key: { to_table: :solid_queue_jobs, on_delete: :cascade }
     t.string "queue_name", null: false
     t.integer "priority", default: 0, null: false
@@ -95,7 +95,7 @@ class CreateSolidQueueTables < ActiveRecord::Migration[7.2]
     t.index [ "scheduled_at", "priority", "job_id" ], name: "index_solid_queue_dispatch_all"
   end
 
-  create_table "solid_queue_semaphores", if_not_exists: true do |t|
+  create_table "solid_queue_semaphores" do |t|
     t.string "key", null: false, index: { unique: true }
     t.integer "value", default: 1, null: false
     t.datetime "expires_at", null: false, index: true
