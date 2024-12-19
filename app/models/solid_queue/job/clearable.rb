8a63da1f0f0835172ad1339c431b32c46262616e
Rosa Gutierrez <rosa@37signals.com>
2024-12-19 19:56:34 +0100

    Add an option to sleep between deleted batches when clearing jobs
    Useful for row-based replication setups where fast, big deletions can
    cause problems.

--- app/models/solid_queue/job/clearable.rb
+++ app/models/solid_queue/job/clearable.rb
@@ -10,9 +10,10 @@ module SolidQueue
       end
 
       class_methods do
-        def clear_finished_in_batches(batch_size: 500, finished_before: SolidQueue.clear_finished_jobs_after.ago, class_name: nil)
+        def clear_finished_in_batches(batch_size: 500, finished_before: SolidQueue.clear_finished_jobs_after.ago, class_name: nil, sleep_between_batches: 0)
           loop do
             records_deleted = clearable(finished_before: finished_before, class_name: class_name).limit(batch_size).delete_all
+            sleep(sleep_between_batches) if sleep_between_batches > 0
             break if records_deleted == 0
           end
         end
