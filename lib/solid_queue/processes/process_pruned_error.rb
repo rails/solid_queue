1b1ab21d26cc89b5ceb123fad8b37c02d81aa577
Elia Schito <elia@schito.me>
2025-02-18 13:05:24 +0100

    Add missing parenthesis

--- lib/solid_queue/processes/process_pruned_error.rb
+++ lib/solid_queue/processes/process_pruned_error.rb
@@ -4,7 +4,7 @@ module SolidQueue
   module Processes
     class ProcessPrunedError < RuntimeError
       def initialize(last_heartbeat_at)
-        super("Process was found dead and pruned (last heartbeat at: #{last_heartbeat_at}")
+        super("Process was found dead and pruned (last heartbeat at: #{last_heartbeat_at})")
       end
     end
   end
