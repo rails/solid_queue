db9c061781b117fcaf18e927e71758d9cb56b48a
Vladyslav Davydenko <vladyslav@hey.com>
2025-06-19 17:35:14 +0300

    Add create and destroy recurring task helpers

--- test/solid_queue_test.rb
+++ test/solid_queue_test.rb
@@ -4,4 +4,31 @@ class SolidQueueTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert SolidQueue::VERSION
   end
+
+  test "creates recurring tasks" do
+    SolidQueue.create_recurring_task("test 1", command: "puts 1", schedule: "every hour")
+    SolidQueue.create_recurring_task("test 2", command: "puts 2", schedule: "every minute", static: true)
+
+    assert SolidQueue::RecurringTask.exists?(key: "test 1", command: "puts 1", schedule: "every hour", static: false)
+    assert SolidQueue::RecurringTask.exists?(key: "test 2", command: "puts 2", schedule: "every minute", static: false)
+  end
+
+  test "destroys recurring tasks" do
+    dynamic_task = SolidQueue::RecurringTask.create!(
+      key: "dynamic", command: "puts 'd'", schedule: "every day", static: false
+    )
+
+    static_task = SolidQueue::RecurringTask.create!(
+      key: "static", command: "puts 's'", schedule: "every week", static: true
+    )
+
+    SolidQueue.destroy_recurring_task(dynamic_task.id)
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      SolidQueue.destroy_recurring_task(static_task.id)
+    end
+
+    assert_not SolidQueue::RecurringTask.exists?(key: "dynamic", static: false)
+    assert SolidQueue::RecurringTask.exists?(key: "static", static: true)
+  end
 end
