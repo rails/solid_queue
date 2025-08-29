49b11ea12a7a5cb70cc39febcba507c263a2d3f1
JP Camara <jp@jpcamara.com>
2025-08-29 01:35:47 -0400

    Refactor internals and api namespace of batches
    * Thanks to Mikael Henriksson for his work in https://github.com/rails/solid_queue/pull/590. His work decentralizes management of batch status by moving it to the BatchUpdateJob, and tracking status using counts rather than querying specific job statuses after the fact. This is a much simpler approach to tracking the jobs, and allows us to avoid a constantly polling set of queries in the dispatcher. Also add in arbitrary metadata to allow tracking data from start to end of execution. This also means enqueueing a BatchUpdateJob based on callbacks in two different kinds of Batchable, which are included when a job is updated and finished, or when a FailedExecution is created (since failed jobs never "finish").
    
    * This batch feature already took some inspiration from the GoodJob batch implementation (https://github.com/bensheldon/good_job). But now we also increase that by adopting some of the buffering and abstractions in a similar form as GoodJob. To discourage heavy reliance on the JobBatch model, it has been renamed to BatchRecord, and a separate Batch interface is how you interact with batches, with some delegation to the core model.
    
    * A new Buffer class (also modeled after GoodJob) was added specifically for batches. This was primarily added to support enqueue_after_transaction_commit. We now override the ActiveJob #enqueue method so we can keep track of which jobs are attempting to enqueue. When enqueue_after_transaction_commit is on, those jobs do not enqueue until all transactions commit. By tracking them at the high level enqueue and keeping a buffer of jobs, we can ensure that the jobs get tracked even when their creation is deferred until the transaction is committed. The side benefit is that we get to enqueue all the jobs together, probably offering some performance advantage. This buffer also keeps track of child batches for the same reason.
    
    * To support triggering a callback/BatchUpdateJob when a job finishes, the update to finished_at needed to become an update! call
    
    * As a simplification, on_failure is now only fired after all jobs finish, rather than at the first time a job fails
    
    * The adapter logic itself also needed to be updated to support the buffer and enqueue_after_transaction_commit. If a job is coming from a batch enqueue, we ignore it here and allow the batching process to enqueue_all at the end of the enqueue block. If the job is originally from a batch, but is retrying, we make sure the job counts in the batch stay updated. I don't love this addition, since it adds alot of complication to the adapter code, all solely oriented around batches
    
    * Batches benefit from keeping jobs until the batch has finished. As such, we ignore the preserve jobs setting, but if it is set to false, we enqueue a cleanup job once the batch has finished and clear out finished jobs
    
    * Implement preserved jobs test and remove todo
    
    * Idempotent updates with pessismistic locks
    
    * Check if it finished before we acquired the lock
    
    * Use enqueue_all directly rather than passing through activejob for completion jobs
    
    Co-authored-by: Mikael Henriksson <mikael@mhenrixon.com>

--- test/integration/batch_lifecycle_test.rb
+++ test/integration/batch_lifecycle_test.rb
@@ -3,26 +3,70 @@
 require "test_helper"
 
 class BatchLifecycleTest < ActiveSupport::TestCase
+  FailingJobError = Class.new(RuntimeError)
+
+  def assert_finished_in_order(*batches)
+    job_batches = batches.map { |batch| SolidQueue::BatchRecord.find_by(batch_id: batch.batch_id) }
+
+    job_batches.each_cons(2) do |batch1, batch2|
+      assert_equal batch1.reload.finished_at < batch2.reload.finished_at, true
+    end
+  end
+
   setup do
+    @_on_thread_error = SolidQueue.on_thread_error
+    SolidQueue.on_thread_error = silent_on_thread_error_for([ FailingJobError ], @_on_thread_error)
     @worker = SolidQueue::Worker.new(queues: "background", threads: 3)
     @dispatcher = SolidQueue::Dispatcher.new(batch_size: 10, polling_interval: 0.2)
   end
 
   teardown do
+    SolidQueue.on_thread_error = @_on_thread_error
     @worker.stop
     @dispatcher.stop
 
     JobBuffer.clear
 
     SolidQueue::Job.destroy_all
-    SolidQueue::JobBatch.destroy_all
+    SolidQueue::BatchRecord.destroy_all
+
+    ApplicationJob.enqueue_after_transaction_commit = false if defined?(ApplicationJob.enqueue_after_transaction_commit)
+    SolidQueue.preserve_finished_jobs = true
   end
 
   class BatchOnSuccessJob < ApplicationJob
     queue_as :background
 
     def perform(batch, custom_message = "")
-      JobBuffer.add "#{custom_message}: #{batch.jobs.size} jobs succeeded!"
+      JobBuffer.add "#{custom_message}: #{batch.completed_jobs} jobs succeeded!"
+    end
+  end
+
+  class BatchOnFailureJob < ApplicationJob
+    queue_as :background
+
+    def perform(batch, custom_message = "")
+      JobBuffer.add "#{custom_message}: #{batch.failed_jobs} jobs failed!"
+    end
+  end
+
+  class FailingJob < ApplicationJob
+    queue_as :background
+
+    retry_on FailingJobError, attempts: 3, wait: 0.1.seconds
+
+    def perform
+      raise FailingJobError, "Failing job"
+    end
+  end
+
+  class DiscardingJob < ApplicationJob
+    queue_as :background
+
+    discard_on FailingJobError
+
+    def perform
+      raise FailingJobError, "Failing job"
     end
   end
 
@@ -33,7 +77,7 @@ class BatchLifecycleTest < ActiveSupport::TestCase
       batch.enqueue do
         AddToBufferJob.perform_later "added from inside 1"
         AddToBufferJob.perform_later "added from inside 2"
-        SolidQueue::JobBatch.enqueue do
+        SolidQueue::Batch.enqueue do
           AddToBufferJob.perform_later "added from inside 3"
         end
       end
@@ -42,10 +86,10 @@ class BatchLifecycleTest < ActiveSupport::TestCase
 
   test "nested batches finish from the inside out" do
     batch2 = batch3 = batch4 = nil
-    batch1 = SolidQueue::JobBatch.enqueue(on_success: BatchOnSuccessJob.new("3")) do
-      batch2 = SolidQueue::JobBatch.enqueue(on_success: BatchOnSuccessJob.new("2")) do
-        batch3 = SolidQueue::JobBatch.enqueue(on_success: BatchOnSuccessJob.new("1")) { }
-        batch4 = SolidQueue::JobBatch.enqueue(on_success: BatchOnSuccessJob.new("1.1")) { }
+    batch1 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("3")) do
+      batch2 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("2")) do
+        batch3 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("1")) { }
+        batch4 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("1.1")) { }
       end
     end
 
@@ -55,17 +99,18 @@ class BatchLifecycleTest < ActiveSupport::TestCase
     wait_for_job_batches_to_finish_for(2.seconds)
     wait_for_jobs_to_finish_for(2.seconds)
 
-    assert_equal [ "1: 0 jobs succeeded!", "1.1: 0 jobs succeeded!", "2: 2 jobs succeeded!", "3: 1 jobs succeeded!" ], JobBuffer.values
-    assert_equal 4, SolidQueue::JobBatch.finished.count
-    assert_equal batch1.reload.finished_at > batch2.reload.finished_at, true
-    assert_equal batch2.finished_at > batch3.reload.finished_at, true
-    assert_equal batch2.finished_at > batch4.reload.finished_at, true
+    expected_values = [ "1: 1 jobs succeeded!", "1.1: 1 jobs succeeded!", "2: 1 jobs succeeded!", "3: 1 jobs succeeded!" ]
+    assert_equal expected_values.sort, JobBuffer.values.sort
+    assert_equal 4, SolidQueue::BatchRecord.finished.count
+    assert_finished_in_order(batch4, batch2, batch1)
+    assert_finished_in_order(batch3, batch2, batch1)
   end
 
   test "all jobs are run, including jobs enqueued inside of other jobs" do
-    SolidQueue::JobBatch.enqueue do
+    batch2 = nil
+    batch1 = SolidQueue::Batch.enqueue do
       AddToBufferJob.perform_later "hey"
-      SolidQueue::JobBatch.enqueue do
+      batch2 = SolidQueue::Batch.enqueue do
         AddToBufferJob.perform_later "ho"
         AddsMoreJobsJob.perform_later
       end
@@ -75,9 +120,182 @@ class BatchLifecycleTest < ActiveSupport::TestCase
     @worker.start
 
     wait_for_job_batches_to_finish_for(2.seconds)
-    wait_for_jobs_to_finish_for(2.seconds)
 
     assert_equal [ "added from inside 1", "added from inside 2", "added from inside 3", "hey", "ho" ], JobBuffer.values.sort
-    assert_equal 3, SolidQueue::JobBatch.finished.count
+    assert_equal 3, SolidQueue::BatchRecord.finished.count
+    assert_finished_in_order(batch2, batch1)
+  end
+
+  test "when self.enqueue_after_transaction_commit = true" do
+    skip if Rails::VERSION::MAJOR == 7 && Rails::VERSION::MINOR == 1
+
+    ApplicationJob.enqueue_after_transaction_commit = true
+    batch1 = batch2 = batch3 = nil
+    JobResult.transaction do
+      JobResult.create!(queue_name: "default", status: "")
+
+      batch1 = SolidQueue::Batch.enqueue do
+        AddToBufferJob.perform_later "hey"
+        JobResult.transaction(requires_new: true) do
+          JobResult.create!(queue_name: "default", status: "")
+          batch2 = SolidQueue::Batch.enqueue do
+            AddToBufferJob.perform_later "ho"
+            batch3 = SolidQueue::Batch.enqueue do
+              AddToBufferJob.perform_later "let's go"
+            end
+          end
+        end
+      end
+    end
+
+    @dispatcher.start
+    @worker.start
+
+    wait_for_job_batches_to_finish_for(2.seconds)
+    wait_for_jobs_to_finish_for(2.seconds)
+
+    assert_equal 3, SolidQueue::BatchRecord.finished.count
+    assert_equal 6, SolidQueue::Job.finished.count
+    assert_equal 6, SolidQueue::Job.count
+    assert_finished_in_order(batch3, batch2, batch1)
+  end
+
+  test "failed jobs fire properly" do
+    batch2 = nil
+    batch1 = SolidQueue::Batch.enqueue(on_failure: BatchOnFailureJob.new("0")) do
+      FailingJob.perform_later
+      batch2 = SolidQueue::Batch.enqueue(on_failure: BatchOnFailureJob.new("1")) do
+        FailingJob.perform_later
+      end
+    end
+
+    @dispatcher.start
+    @worker.start
+
+    wait_for_job_batches_to_finish_for(3.seconds)
+    wait_for_jobs_to_finish_for(3.seconds)
+
+    job_batch1 = SolidQueue::BatchRecord.find_by(batch_id: batch1.batch_id)
+    job_batch2 = SolidQueue::BatchRecord.find_by(batch_id: batch2.batch_id)
+
+    assert_equal 2, SolidQueue::BatchRecord.count
+    assert_equal 2, SolidQueue::BatchRecord.finished.count
+
+    assert_equal 3, job_batch1.total_jobs
+    assert_equal 1, job_batch1.failed_jobs
+    assert_equal 2, job_batch1.completed_jobs
+    assert_equal 0, job_batch1.pending_jobs
+
+    assert_equal 3, job_batch2.total_jobs
+    assert_equal 1, job_batch2.failed_jobs
+    assert_equal 2, job_batch2.completed_jobs
+    assert_equal 0, job_batch2.pending_jobs
+
+    assert_equal [ "failed", "failed" ].sort, SolidQueue::BatchRecord.all.pluck(:status)
+    assert_equal [ "0: 1 jobs failed!", "1: 1 jobs failed!" ], JobBuffer.values.sort
+    assert_finished_in_order(batch2, batch1)
+  end
+
+  test "discarded jobs fire properly" do
+    batch2 = nil
+    batch1 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("0")) do
+      DiscardingJob.perform_later
+      batch2 = SolidQueue::Batch.enqueue(on_success: BatchOnSuccessJob.new("1")) do
+        DiscardingJob.perform_later
+      end
+    end
+
+    @dispatcher.start
+    @worker.start
+
+    wait_for_job_batches_to_finish_for(3.seconds)
+    wait_for_jobs_to_finish_for(3.seconds)
+
+    job_batch1 = SolidQueue::BatchRecord.find_by(batch_id: batch1.batch_id)
+    job_batch2 = SolidQueue::BatchRecord.find_by(batch_id: batch2.batch_id)
+
+    assert_equal 2, SolidQueue::BatchRecord.count
+    assert_equal 2, SolidQueue::BatchRecord.finished.count
+
+    assert_equal 1, job_batch1.total_jobs
+    assert_equal 0, job_batch1.failed_jobs
+    assert_equal 1, job_batch1.completed_jobs
+    assert_equal 0, job_batch1.pending_jobs
+
+    assert_equal 1, job_batch2.total_jobs
+    assert_equal 0, job_batch2.failed_jobs
+    assert_equal 1, job_batch2.completed_jobs
+    assert_equal 0, job_batch2.pending_jobs
+
+    assert_equal [ "completed", "completed" ].sort, SolidQueue::BatchRecord.all.pluck(:status)
+    assert_equal [ "0: 1 jobs succeeded!", "1: 1 jobs succeeded!" ], JobBuffer.values.sort
+    assert_finished_in_order(batch2, batch1)
+  end
+
+  test "preserve_finished_jobs = false" do
+    SolidQueue.preserve_finished_jobs = false
+    batch1 = SolidQueue::Batch.enqueue do
+      AddToBufferJob.perform_later "hey"
+    end
+
+    assert_equal false, batch1.reload.finished?
+    assert_equal 1, SolidQueue::Job.count
+    assert_equal 0, SolidQueue::Job.finished.count
+
+    @dispatcher.start
+    @worker.start
+
+    wait_for_job_batches_to_finish_for(2.seconds)
+    wait_for_jobs_to_finish_for(2.seconds)
+
+    assert_equal true, batch1.reload.finished?
+    assert_equal 0, SolidQueue::Job.count
+  end
+
+  test "batch interface" do
+    batch = SolidQueue::Batch.enqueue(
+      metadata: { source: "test", priority: "high", user_id: 123 },
+      on_finish: OnFinishJob,
+      on_success: OnSuccessJob,
+      on_failure: OnFailureJob
+    ) do
+      AddToBufferJob.perform_later "hey"
+    end
+
+    @dispatcher.start
+    @worker.start
+
+    wait_for_job_batches_to_finish_for(2.seconds)
+    wait_for_jobs_to_finish_for(2.seconds)
+
+    assert_equal [ "Hi finish #{batch.batch_id}!", "Hi success #{batch.batch_id}!", "hey" ].sort, JobBuffer.values.sort
+    assert_equal 1, batch.reload.completed_jobs
+    assert_equal 0, batch.failed_jobs
+    assert_equal 0, batch.pending_jobs
+    assert_equal 1, batch.total_jobs
+  end
+
+  class OnFinishJob < ApplicationJob
+    queue_as :background
+
+    def perform(batch)
+      JobBuffer.add "Hi finish #{batch.batch_id}!"
+    end
+  end
+
+  class OnSuccessJob < ApplicationJob
+    queue_as :background
+
+    def perform(batch)
+      JobBuffer.add "Hi success #{batch.batch_id}!"
+    end
+  end
+
+  class OnFailureJob < ApplicationJob
+    queue_as :background
+
+    def perform(batch)
+      JobBuffer.add "Hi failure #{batch.batch_id}!"
+    end
   end
 end
