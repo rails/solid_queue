f196073c69658062432b7eb1c1e70eaa61d262e4
JP Camara <jp@jpcamara.com>
2025-09-05 17:54:25 -0400

    Move away from a batch_processed_at to batch_execution model
    * BatchExecution allows us to know for sure we only ever run completion on a job once. We destroy it and update the counts in a transaction. Also can remove the batch_processed_at field from jobs, which are meant to be touched as little as possible and relevant states reflected in *_execution models
    
    * It also gives us a slightly cleaner interface in the batchable classes
    
    * Updated some table naming and pruned unused fields/indexes
    
    * Increase child batch count as new batches are enqueued, even in existing batches
    
    * Refactor to a unified Batch interface
    
    *   It was overly complicated to split Batch and BatchRecord apart just to keep a more strict interface
    
    *   That concept was taken from GoodJob, but it didn't feel in the spirit of the simplicity of the SolidQueue project. It was alot of concepts to juggle in your head
    
    *   Also moved around some files (like the cleanup and empty jobs) to the more appropriate app/jobs

--- app/models/solid_queue/job/batchable.rb
+++ app/models/solid_queue/job/batchable.rb
@@ -6,7 +6,8 @@ module SolidQueue
       extend ActiveSupport::Concern
 
       included do
-        belongs_to :job_batch, foreign_key: :batch_id, optional: true
+        belongs_to :batch, foreign_key: :batch_id, primary_key: :batch_id, class_name: "SolidQueue::Batch", optional: true
+        has_one :batch_execution, foreign_key: :job_id, dependent: :destroy
 
         after_update :update_batch_progress, if: :batch_id?
       end
@@ -16,9 +17,11 @@ module SolidQueue
           return unless saved_change_to_finished_at? && finished_at.present?
           return unless batch_id.present?
 
-          BatchUpdateJob.perform_later(batch_id, self)
+          # Jobs marked as finished are considered completed
+          # (even if they failed and are being retried - we don't know that here)
+          BatchExecution.process_job_completion(self, "completed")
         rescue => e
-          Rails.logger.error "[SolidQueue] Failed to notify batch #{batch_id} about job #{id} completion: #{e.message}"
+          Rails.logger.error "[SolidQueue] Failed to update batch #{batch_id} progress for job #{id}: #{e.message}"
         end
     end
   end
